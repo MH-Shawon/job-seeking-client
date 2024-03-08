0c587b5073e50ded12ca6fc18a545f15cea6dc5c
Mohsin <mohsin@localhost.localdomain>
2024-03-09 01:28:57 +0600

    banner description added

--- src/Components/Layouts/Banner/Banner.jsx
+++ src/Components/Layouts/Banner/Banner.jsx
@@ -1,31 +1,36 @@
 import { useState } from "react";
-import banner from '../../../assets/Banner/banner1.jpg'
-import { BiSearch } from 'react-icons/bi';
+import banner from "../../../assets/Banner/banner1.jpg";
+import { BiSearch } from "react-icons/bi";
 const Banner = () => {
-    const [searchValue, setSearchValue] = useState('');
+    const [searchValue, setSearchValue] = useState("");
 
     const handleSearchChange = (e) => {
         setSearchValue(e.target.value);
     };
 
     const handleSearchSubmit = (e) => {
-        e.preventDefault();console.log('Search submitted:', searchValue);
+        e.preventDefault();
+        console.log("Search submitted:", searchValue);
     };
 
     return (
-        <div className="banner">
+        <div className="banner ">
             <div className="relative grid h-[28rem] w-full max-w-full flex-col items-end justify-center overflow-hidden rounded bg-white bg-clip-border text-center text-gray-700">
                 <div
                     style={{ backgroundImage: `url(${banner})` }}
-                    className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
-                    <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
+                    className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-cover bg-clip-border bg-center text-gray-700 shadow-none"
+                >
+                    <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-[#151515] to-[rgba(21, 21, 21, 0)]"></div>
                 </div>
-                <div className="relative my-auto md:px-12">
-                    
-                    <h5 className="block mb-4 font-sans text-4xl antialiased font-semibold leading-snug tracking-normal text-gray-300">
+                <div className="relative my-auto md:px-12 ">
+                    <h5 className="block font-sans text-4xl antialiased font-semibold leading-snug tracking-tight text-gray-300">
                         Find Your Dream Job
                     </h5>
-                    <form onSubmit={handleSearchSubmit} className="mt-6 flex items-center justify-center">
+                    <p className="text-white text-lg">Your next adventure awaits. Search jobs & launch your career.</p>
+                    <form
+                        onSubmit={handleSearchSubmit}
+                        className="mt-5 flex items-center justify-center"
+                    >
                         <input
                             type="text"
                             value={searchValue}
@@ -33,15 +38,16 @@ const Banner = () => {
                             placeholder="Search..."
                             className="mr-2 p-2 border border-gray-300 rounded "
                         />
-                        <button type="submit" className="p-2 bg-gray-800 text-white rounded">
+                        <button
+                            type="submit"
+                            className="p-2 bg-gray-800 text-white rounded"
+                        >
                             <BiSearch />
                         </button>
                     </form>
-                    
-                    
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 export default Banner;
