e0e5a188a59322e133ab8be05ac8dfab609a20d4
Mohsin <mohsin@localhost.localdomain>
2024-03-09 23:37:56 +0600

    footer added

--- src/Components/Layouts/MainLayouts.jsx
+++ src/Components/Layouts/MainLayouts.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/SharedPages/NavBar/Navbar";
 import { Toaster } from "react-hot-toast";
+import Footer from "../Pages/SharedPages/Footer/Footer";
 
 const MainLayouts=()=>{
     return(
         <div>
             <Navbar />
              <Outlet />
+             <Footer />
             <Toaster />
         </div>
     )}
